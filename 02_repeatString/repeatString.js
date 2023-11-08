b96f8311a7f49e7a85cbfcaf994d9def36106e42
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-11-08 20:52:43 +0530

    Finished first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringinput,numberOfTimesToRepeatTheString) {
+    if(numberOfTimesToRepeatTheString<0){
+        return "ERROR";
+    }
+    let output='';
+    for(let i=0;i<numberOfTimesToRepeatTheString;++i){
+        output += stringinput;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputStringToBeReversed) {
+    let temporaryString = '';
+    for(let i=inputStringToBeReversed.length-1;i>=0;--i){
+        temporaryString += inputStringToBeReversed[i];
+    }
+    return temporaryString;
 };
 
 // Do not edit below this line
