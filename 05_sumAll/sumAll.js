bb19cbd4697e6c19e1131e5f1dff1979cff0b5ff
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-11-09 20:09:23 +0530

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if(typeof(end)!== 'number' || typeof(start)!== 'number'){
+        return 'ERROR';
+    }
+    if(start<0||end<0){
+        return 'ERROR';
+    }
+    
+    if(start>end){
+        bigger=start;
+        smaller=end;
+    }else{
+        bigger=end;
+        smaller=start;
+    }
 
+    let sum = 0;
+    for(let i=smaller;i<=bigger;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
