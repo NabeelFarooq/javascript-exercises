e2cdff00032796b8666aadc5b1f855a8e76be836
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-11-09 19:43:28 +0530

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...stuffToBeRemoved) {
+    let numberOfItemsToBeRemoved = stuffToBeRemoved.length;
+    for(let i=0;i<numberOfItemsToBeRemoved;i++){
+        let index = array.indexOf(stuffToBeRemoved[i]);
+        if(index > -1){
+            array.splice(index,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
