2ecba19695337f4114adde8b1945a4df30062c86
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-11-10 20:04:25 +0530

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remove punctuation and space from string
+    string = string.replace(/[!"#$%&'()" "*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    // make string lowercase to make it case-insensitive
+    string = string.toLowerCase();
+    let i = 0;
+    let j = string.length - 1;
+    for(;i<string.length && j>=0;++i,j--){
+        if(string[i]!=string[j]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
