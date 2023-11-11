52febd6bb00a045739305396918f7e8daafa9758
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-11-11 08:56:08 +0530

    Push the code that passes 2/3 test cases for 12_findTheOldest exercise, second test case failing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(peopleArray) {
+    const OldestPersonObject = peopleArray.reduce((overAllReturn,currentObject) => {
+        if(!currentObject.yearOfDeath){
+            let currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+            yearOfDeath = currentYear;
+        }
+        let age = currentObject.yearOfDeath - currentObject.yearOfBirth;
+        if(age>(overAllReturn.yearOfDeath-overAllReturn.yearOfBirth)){
+            overAllReturn = currentObject;
+        }
+        return overAllReturn;
+    });
+    return OldestPersonObject;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
